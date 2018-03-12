29ee349b43a3f9154d289d46019329d2980dc131
kr620357 <kr620357@KRDKD8NETI016.na.intranet.msd>
2018-03-12 17:52:53 +0900

    카테고리 메뉴 수정

--- storeFront/work/scripts/akl_common.js
+++ storeFront/work/scripts/akl_common.js
@@ -1812,3 +1812,32 @@ function layerPopOver( btnOpenLayer , targetLayer ){
 	});
 	
 })(jQuery, undefined);
+
+
+//2018.03.12 카테고리 왼쪽메뉴
+$(function() {
+	var Accordion = function(el, multiple) {
+		this.el = el || {};
+		this.multiple = multiple || false;
+
+		// Variables privadas
+		var links = this.el.find('.link');
+		// Evento
+		links.on('click', {el: this.el, multiple: this.multiple}, this.dropdown)
+	}
+
+	Accordion.prototype.dropdown = function(e) {
+		var $el = e.data.el;
+			$this = $(this),
+			$next = $this.next();
+
+		$next.slideToggle();
+		$this.parent().toggleClass('open');
+
+		if (!e.data.multiple) {
+			$el.find('.submenu').not($next).slideUp().parent().removeClass('open');
+		};
+	}	
+
+	var accordion = new Accordion($('#accordion'), false);
+});
