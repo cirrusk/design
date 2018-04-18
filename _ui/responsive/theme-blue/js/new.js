fa66cf444995548a121711dd1abeb71a531430f9
kr620357 <kr620357@KRDKD8NETI016.na.intranet.msd>
2018-04-18 12:25:37 +0900

    공지사항, 뉴스룸 작업

--- _ui/responsive/theme-blue/js/new.js
+++ _ui/responsive/theme-blue/js/new.js
@@ -415,3 +415,58 @@ $(function(){
 	});
 
 });
+
+//공지사항 2018.04.17
+$(function() {
+	$(".js-order-history-filters").click(function(){
+		$(".js-search-form-wrapper").addClass('hidden-sm hidden-xs');
+		if($(".js-filter-form-wrapper").hasClass('hidden-sm hidden-xs')){
+			$(".js-filter-form-wrapper").removeClass('hidden-sm hidden-xs');
+		}else{
+			$(".js-filter-form-wrapper").addClass('hidden-sm hidden-xs');
+		}
+	});
+
+	$(".js-order-history-search").click(function(){
+		$(".js-filter-form-wrapper").addClass('hidden-sm hidden-xs');
+		if($(".js-search-form-wrapper").hasClass('hidden-sm hidden-xs')){
+			$(".js-search-form-wrapper").removeClass('hidden-sm hidden-xs');
+		}else{
+			$(".js-search-form-wrapper").addClass('hidden-sm hidden-xs');
+		}
+	});
+
+	//new-announcement-pop
+	$('.ditto-schedule-table tbody tr').click(function(event) {
+		event.preventDefault();
+		$(".message-center-pop2,.overlay").show();
+	});
+	$(".message-center-pop2 #cboxClose").click(function() {
+		$(".message-center-pop2,.overlay").hide();
+	});
+
+
+	//mob get ".tabs-toggles" max-width
+	function tabwidth(){
+		var maxWidth=$(window).width()+4;
+		$(".tabs-toggles").css('max-width',maxWidth)
+	}
+	$(window).resize(function(){
+		tabwidth()
+	});
+	$(document).ready(function(){
+		tabwidth()
+	})
+
+
+	$(".url-copy").click(function(){
+		$(".url_pop").toggle();
+	});
+
+	$(".cart-popup-close").click(function(){
+		$(".url_pop").fadeOut();
+		$(".url_pop").fadeOut("slow");
+		$(".url_pop").fadeOut(3000);
+	});
+
+});
