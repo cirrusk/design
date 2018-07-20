86f8fe966453ff36c396825e7d41f66ea9c53d02
kr620354 <kr620354@krvdn8neti002.na.intranet.msd>
2018-07-20 13:29:36 +0900

    SOP 테스트용

--- _ui/responsive/theme-blue/js/akl_sopTemp.js
+++ _ui/responsive/theme-blue/js/akl_sopTemp.js
@@ -1,5 +1,33 @@
 /* 코어 SOP 스크립트 정리 */
 $(function(){
+
+	//레이어 띄우기
+	$('#addNewPayment').on('click',function(){
+		layerPopOver(this,'#pop-payment');
+	});
+
+	$('#addNewAddress').on('click',function(){
+		layerPopOver(this,'#pop-address');
+	});
+
+	$('.del-profile').on('click',function(){
+		layerPopOver(this,'#del-profile');
+	});
+
+	$('#btnEditSave').on('click',function(){
+		layerPopOver(this,'#save-change');
+	});
+
+	$('#btnEditCancel').on('click',function(){
+		layerPopOver(this,'#cancel-change');
+
+	});
+
+	$('.mysop-list-cancel').click(function(){
+		layerPopOver(this,'#mysop-list-cancel');
+	});
+
+
 	//
 	SOP_editProfile();
 });
@@ -26,7 +54,6 @@ function SOP_editProfile(){
 
 
 /* -------------------- 이하 정리예정 ------------------- */
-
 (function () {
 	function listenSearchInput() {
 		if ($(this).val().length >= 1) {
@@ -43,9 +70,9 @@ function SOP_editProfile(){
 
 })();
 $("#editpay-card-password").bind("input propertychange",
-	function () {
+	function() {
 		value = $('#editpay-card-password').val();
-		setTimeout(function () {
+		setTimeout(function() {
 				str = value.substr(value.length - 1, 1);
 				str = '*';
 				str1 = '';
@@ -62,9 +89,9 @@ $("#editpay-card-password").bind("input propertychange",
 			0);
 	});
 $("#editpay-card-cvv").bind("input propertychange",
-	function () {
+	function() {
 		value = $('#editpay-card-cvv').val();
-		setTimeout(function () {
+		setTimeout(function() {
 				str = value.substr(value.length - 1, 1);
 				str = '*';
 				str1 = '';
@@ -81,18 +108,27 @@ $("#editpay-card-cvv").bind("input propertychange",
 			0);
 	});
 
-function verify(){
-	var day = $("#process-day").val();
-	if (day == '1'){
-		$(".verify-form").addClass("has-error");
-		$(".warning-tip").show();
-		return;
-	} else {
-		$("#save-change").fadeIn();
+
+(function () {
+
+	function inserQTY() {
+		var w = $(window).width();
+		var tagert = $(".mysop-list .list-row-content .col-md-2");
+		if (w < 768) {
+			for(var i=0;i< tagert.length;i++){
+				if(tagert.eq(i).find("label").length< 2){
+					tagert.eq(i).prepend('<label class="qty">QTY</label>');
+				}
+			}
+		}else{
+			$(".qty").remove();
+		}
 	}
+	inserQTY();
+})();
 
 
-}
+
 
 function changeFunction(){
 	var inputValue = $("#quick-shop-search").val().trim();
@@ -113,7 +149,8 @@ function changeFunction(){
 
 	}
 }
-$(function () {
+$(function(){
+
 	$('#view-month').change(function() {
 
 		var valueMonth = $("#view-month").val();
@@ -124,82 +161,141 @@ $(function () {
 		}
 	});
 
-
-	$('.pop-close').click(function () {
+	$('.pop-close').click(function(){
 		$(".popup").hide();
 	});
 
-
-	$('.pop-close').click(function () {
-		$(".popup").hide();
-	});
-
-	$('.del-profile').click(function (e) {
+	$('.del-profile').click(function(e) {
 		e.preventDefault();
 		$("#del-profile").show();
 
 	});
 
-	$('.mysop-list-cancel').click(function (e) {
-		e.preventDefault();
-		$("#mysop-list-cancel").show();
-
-	});
 
 	$("#sortOptions2").change(function () {
 		var ss = $(this).children('option:selected').val();
 		if (ss == "Inactive") {
 			$("#sortOptions1").addClass("select-close");
-			$("#sortOptions1").attr("disabled", true);
+			$("#sortOptions1").attr("disabled",true);
 			$("#active-block").hide();
 			$("#inactive-block").show();
+			$("#cancel-block").hide();
 
 		} else if (ss == "Active") {
 			$("#sortOptions1").removeClass("select-close");
-			$("#sortOptions1").attr("disabled", false);
-
-
+			$("#sortOptions1").attr("disabled",false);
 			$("#active-block").show();
+			$("#inactive-block").hide();
+			$("#cancel-block").hide();
+		}else{
+			$("#cancel-block").show();
+			$("#active-block").hide();
 			$("#inactive-block").hide();
 		}
 	});
-	$('.arrow-li-prev').click(function (e) {
+	$(".inactive").on("click",function (e) {
 		e.preventDefault();
-		$('.months-select-bar .active').prev("li").children("a").tab('show');
+	   $("#sortOptions1").addClass("select-close");
+			$("#sortOptions1").attr("disabled",true);
+			$("#active-block").hide();
+			$("#inactive-block").show();
+			$("#cancel-block").hide();
+	});
+	$(".active").on("click",function (e) {
+		e.preventDefault();
+		$("#sortOptions1").removeClass("select-close");
+			$("#sortOptions1").attr("disabled",false);
+			$("#active-block").show();
+			$("#inactive-block").hide();
+			$("#cancel-block").hide();
+	});
+	$(".cancel").on("click",function (e) {
+		e.preventDefault();
+		$("#cancel-block").show();
+			$("#active-block").hide();
+			$("#inactive-block").hide();
 	});
 
-	$('.arrow-li-next').click(function (e) {
-		e.preventDefault();
-		$('.months-select-bar .active').next("li").children("a").tab('show');
-	});
 
-	$('#addNewAddress').click(function (e) {
-		e.preventDefault();
-		$('#pop-address').fadeIn();
-	});
-	$('#addNewPayment').click(function (e) {
+	/*
+	$('#addNewAddress').click(function(e) {
+				e.preventDefault();
+				$('#pop-address').fadeIn();
+			});
+	$('#addNewPayment').click(function(e) {
 		e.preventDefault();
 		$('#pop-payment').fadeIn();
 	});
-
-	$('.pop-close, #submit-btn, #cancel-btn').click(function (e) {
+	$('.pop-close, #submit-btn, #cancel-btn').click(function(e) {
 		e.preventDefault();
 		$('.popup').fadeOut();
 	});
-	/*
-	$('.close-edit').click(function (e) {
+	$('.close-edit').click(function(e) {
 		e.preventDefault();
 		$('.edit-block').slideUp();
 		$(".downline-profile-second-line").fadeIn(800);
-	});*/
+	});
+	*/
 
+	$('body').delegate('.list-total .opera','click',function(){
+		var count = $(this).attr("data");
+		var inputNum = $(this).parent().children("#amount-cal-input");
+		inputNum.val(function(){
+			var value=$(this).val();
+			count=="-"?value--:value++;
+			if(value>=20){
+				$(".list-total .icon-add").removeClass("opera");
+				$(this).parent('.amount-cal').addClass('limit-num');
+				$(this).parents('.product__list--item').addClass('has-limit');
+				$(this).parents('.product__list--item').find('.non-click-panel').show();
+				var value =20;
+				return value;
+			}
+			else if(value>1){
+				$(this).parent('.amount-cal').removeClass('limit-num');
+				$(".list-total .icon-del").addClass("opera");
+				$(".list-total .icon-add").addClass("opera");
+				$(this).parents('.product__list--item').removeClass('has-limit');
+				$(this).parents('.product__list--item').find('.non-click-panel').hide();
+				return value;
+			}
+			else if(value<=1){
+				$(".list-total .icon-del").removeClass("opera");
+				$(this).parent('.amount-cal').addClass('limit-num');
+				$(this).parents('.product__list--item').addClass('has-limit');
+				$(this).parents('.product__list--item').find('.non-click-panel').show();
+				var value =1;
+				return value;
+			}
+			else if(1<value<20){
+				$(this).parents('.product__list--item').find('.non-click-panel').hide();
+				$(this).parents('.product__list--item').removeClass('has-limit');
+				$(".list-total .icon-add").addClass("opera");
+				$(this).parent('.amount-cal').removeClass('limit-num');
+				return value;
+			}
+		})
+	});
 	//reset
-	$('.reset').click(function () {
+	$('.reset').click(function(){
 		$(this).parents('#quickShopForm').find('#quick-shop-search').val('');
 		$(this).parents('#quickShopForm').next('#quick-search-list').slideUp(1000);
 	});
 
-/*
+	/*
+	$('#edit-profile').click(function(e){
+		e.preventDefault();
+		if($(".edit-block").is(':hidden')){
+			$(".downline-profile-second-line").hide();
+
+			$(".edit-block").slideDown();
+		}else{
+			$(".edit-block").hide();
+			$(".downline-profile-second-line").show();
+		}
+
+	});
+
 	var owl2 = $('#productSuggestListTabs');
 	owl2.owlCarousel({
 		margin: 0,
@@ -207,16 +303,16 @@ $(function () {
 		dots: false,
 		loop: true,
 		responsive: {
-			0: {
-				items: 1
-			},
-			768: {
-				items: 2
-			},
-			960: {
-				items: 5
-			}
+		  0: {
+			items: 1
+		  },
+		  768: {
+			items: 2
+		  },
+		  960: {
+			items: 5
+		  }
 		}
-
-	});*/
-})
+	});
+	*/
+});
