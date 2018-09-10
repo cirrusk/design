7506e7f57d52037c13682d0c27035fd95caac788
kr620354 <kr620354@krvdn8neti002.na.intranet.msd>
2018-09-10 12:03:46 +0900

    jQuery CSS Customizable Scrollbar 플러그인 추가

--- _ui/responsive/theme-blue/js/akl_contents.js
+++ _ui/responsive/theme-blue/js/akl_contents.js
@@ -2,21 +2,24 @@ $(function(){
 /** ----- 브랜드 ----- */
 	//브랜드 상단 SNS
 	brandTopSNS();
-	//XS에너지 홈 
+
+	//XS에너지 홈
 	xsEnergyHome();
-	
+
 	$(window).resize(function(){
 		brandTopSNS();
 		xsEnergyHome();
 	});
 
+	//연혁
+	timelineFunc();
 });
 
 /* 브랜드 모바일 SNS 위치 */
 function brandTopSNS(){
 	var $brandHeading = $('.brand .top-title-wrapper');
 	if(!$brandHeading.length){return;}
-	
+
 	var $brandSns = $('.brand .bod-view-sns');
 	var titHeight = $brandHeading.outerHeight();
 	var windowWidth = $(window).width();
@@ -39,24 +42,47 @@ function brandTopSNS(){
 }
 /* XS에너지 홈 */
 function xsEnergyHome(){
-	
+
 	var $xsenergyList = $('.xsenergy_mainlist');
 	if(!$xsenergyList.length){return;}
-	
+
 	var colmn = $xsenergyList.children('li:nth-last-child(2)');
 	var colmnHeight = colmn.height() + 0.01;
 	var windowWidth = $(window).width();
-	
-	
+
+
 	function colmnTop(){
 		colmn.css('margin-top',-colmnHeight+'px');
 	}
 	//console.log(colmnHeight);
-	
+
 	if(windowWidth > 1024) {
 		colmnTop();
 	}
 	else{
 		colmn.css('margin-top','0');
+	}
+}
+
+/* 연혁 */
+function timelineFunc(){
+	$('.timelineWrap').owlCarousel({
+		loop: false,
+		nav: true,
+		dots: true,
+		margin: 0,
+
+		responsive: {
+			768 : {
+				items: 1
+			},
+			1200 : {
+				items: 1
+			}
+		}
+	});
+
+	if($('.timeline .scrollbar-inner').length){
+		$('.scrollbar-inner').scrollbar(); //스크롤바 플러그인 실행
 	}
 }
